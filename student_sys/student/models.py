71bd4746e7a4ed013ded3a0a7cd5907df2c20203
vashly <curesoul@163.com>
2022-07-27 14:03:38 +0800

    3

--- student_sys/student/models.py
+++ student_sys/student/models.py
@@ -12,4 +12,19 @@ class Student(models.Model):
         (1, 'allow'),
         (2, 'deny'),
     ]
+    name = models.CharField(max_length=128, verbose_name="姓名")
+    sex = models.IntegerField(choices=SEX_ITEMS, verbose_name="性别")
+    profession = models.CharField(max_length=128, verbose_name="职业")
+    email = models.EmailField(verbose_name="邮件")
+    qq = models.CharField(max_length=128, verbose_name="qq")
+    phone = models.CharField(max_length=128, verbose_name="手机")
 
+    status = models.IntegerField(choices=STATUS_ITEMS, default=0,verbose_name="审核状态")
+
+    created_time = models.DateTimeField(auto_now_add=True, editable=False, verbose_name="创建时间")
+
+    def __str__(self):
+        return '<Student:{}>'.format(self.name)
+
+    class Meta:
+        verbose_name = verbose_name_plural = "学员信息"
